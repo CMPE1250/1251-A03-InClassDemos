7d0f02002e466e35fd2c0e210d73702fb541ab15
AJ Armstrong <aja@nait.ca>
2025-10-26 12:25:07 -0600

    Added dummy project for first USART demo

---
+++ 007-USARTDemo1/main.c
@@ -0,0 +1,32 @@
+/*********************************************************************
+*                  DEMO 7 - Building a USART Library                 *
+*                          AJ Armstrong, CNT                         *
+**********************************************************************
+
+-------------------------- END-OF-HEADER -----------------------------
+
+File    : main.c
+Purpose : Generic application start
+
+*/
+
+#include <gpio.h>
+#include <usart.h>
+
+/*********************************************************************
+*
+*       main()
+*
+*  Function description
+*   Application entry point.
+*/
+int main(void) {
+  int count = 0; // Loop Counter
+  for(;;)
+  {
+
+    ++count;
+  }
+}
+
+/*************************** End of file ****************************/
