be5a4914eeb7fe27c494b008943f37106fdbebe4
AJ Armstrong <aja@nait.ca>
2025-10-27 14:59:15 -0600

    Tweaks

--- 007-USARTDemo1/main.c
+++ 007-USARTDemo1/main.c
@@ -12,6 +12,7 @@ Purpose : Generic application start
 
 #include <gpio.h>
 #include <usart.h>
+#include <stdio.h>
 
 /*********************************************************************
 *
@@ -22,13 +23,17 @@ Purpose : Generic application start
 */
 int main(void) {
   int count = 0; // Loop Counter
+  byte c = 0;
 
   //Fire up the main console
   _USART_Init_USART2(SystemCoreClock, 38400);
 
   for(;;)
   {
-
+    utiDelay(500);
+    _USART_TxByte(USART2, 'A');
+    if(_USART_RxByte(USART2, &c))
+      printf("%c...",c);
     ++count;
   }
 }
