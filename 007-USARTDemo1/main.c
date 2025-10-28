ad1482b04140f65282ca1d10d3d7c050d84109f4
AJ Armstrong <aja@nait.ca>
2025-10-28 08:33:14 -0600

    Update main.c
    ICA 5, PART A Complete

--- 007-USARTDemo1/main.c
+++ 007-USARTDemo1/main.c
@@ -25,15 +25,18 @@ int main(void) {
   int count = 0; // Loop Counter
   byte c = 0;
 
+  //User LED (PC6) and PB0 Output
+  //Remember that my SetPinMode (and SetAltertnate) start the clocks.
+  _GPIO_SetPinMode(GPIOC, 6, _GPIO_PinMode_Output);
+  _GPIO_SetPinMode(GPIOB, 0, _GPIO_PinMode_Output);
+
   //Fire up the main console
   _USART_Init_USART2(SystemCoreClock, 38400);
 
   for(;;)
   {
-    utiDelay(10);
-    _USART_TxByte(USART2, 'A');
-    if(_USART_RxByte(USART2, &c))
-      printf("%c...",c);
+    utiDelay(200);
+    _USART_TxByte(USART2, '.');
     ++count;
   }
 }
