9e8602e264d70cf8dae1ff1d4280361d0709b48a
AJ Armstrong <aja@nait.ca>
2025-11-04 10:23:15 -0700

    Feat: Implement initial USART display and input demo functionality

--- 008-USARTDemo2/main.c
+++ 008-USARTDemo2/main.c
@@ -24,25 +24,35 @@ Purpose : Generic application start
 */
 int main(void) {
   int count = 0; //Loop Counter
-
-  _USART_Init_USART2(SystemCoreClock, 38400);
-
-  _USART_TxStringXY(USART2, 7, 7, "AJ ARMSTRONG");
+  char szName[] = "AJ Armstrong";
+  char szCourse[] = "CMPE2150";
+  char szBuffer[_USART_BUFFSIZE] = {0}; //Standard buffer
   
   //Part A
 
   //In the one-time initializations section of your code, bring up USART2 at 
     //38400 BAUD. 
+  _USART_Init_USART2(SystemCoreClock, 38400);
 
-  //Clear the terminal screen.
+  //Clear the terminal screen. 
+  _USART_ClearScreen(USART2);
   //Print your full name in the terminal aligned to the upper-left corner of a 
     //default 80x24 terminal.
+  // Already at home, so...
+  _USART_TxString(USART2, szName);
   //Print CMPE1250 aligned to the bottom-right corner of a default 80x24 terminal.
+  //_USART_ROWS is declared in my header as 28, _USART_COLS as 80
+  _USART_TxStringXY(USART2,_USART_COLS - strlen(szCourse),_USART_ROWS,szCourse);
 
+  _USART_GotoXY(USART2, 2, 2);
+  _USART_RxString(USART2, szBuffer, _USART_BUFFSIZE, _USART_RX_ENFORCE_HEX);
+  _USART_TxStringXY(USART2, 5, 5, szBuffer);
+  
     
   //Event Loop
   for(;;)
   {
+
 
     count ++; // Increment loop counter
   }
