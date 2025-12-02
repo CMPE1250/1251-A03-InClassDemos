5f1639ff3df0868ad49282e67f63c268b527eb90
Carlos Estay - NAIT <75865555+carlosestay@users.noreply.github.com>
2025-12-02 09:29:00 -0700

    pushed demos

--- 015-PWM-Demo1/main.c
+++ 015-PWM-Demo1/main.c
@@ -51,19 +51,27 @@ int main(void)
  
   printf("SYSCLK = %lu Hz\n", SystemCoreClock);
 
+  /*Set PA7 to alternate function TIM14->CH1*/
+  _GPIO_SetPinMode(GPIOA, 7,  _GPIO_PinMode_AlternateFunction);
+  _GPIO_SetPinAlternateFunction(GPIOA, 7, 4); //AF4 for TIM14_CH1
   _GPIO_SetPinMode(GPIOB, 3,  _GPIO_PinMode_Output);
 
   /*Disable timer, in case already enabled*/
   TIM14->CR1 &= ~TIM_CR1_CEN;
   /*Set prescaler to 40-> TIck @ 1[MHz]*/
   TIM14->PSC = 40 - 1;
-  /*Set Autoreload in ARR register*/
-  TIM14->ARR = 250 - 1; //ticks in [us] //mod-250 counter
+  /*Set Autoreload in ARR register: period*/
+  TIM14->ARR = 1000 - 1; //ticks in [us] //mod-1000 counter
+  /*POsitive duty*/
+  TIM14->CCR1 = 250; //250 ticks -> 250[us] -> 25% DC
 
-  /*OUTPUT COMPARE SETTINGS*/
-  TIM14->CCR1 = 50; //50 ticks -> 50[us]
+  /*Enable TIM14 CH1*/
+  TIM14->CCER |= TIM_CCER_CC1E;
 
-
+  /*PWM Mopde 1 for CCR1*/
+  TIM14->CCMR1 &= ~TIM_CCMR1_OC1M;//Clear Output comare mode bits
+  TIM14->CCMR1 |= TIM_CCMR1_OC1M_2 | TIM_CCMR1_OC1M_1;
+  //TIM14->CCMR1 |= 0b110 << 4;  //same as above
 
   /*Enable counter*/
   TIM14->CR1 |= TIM_CR1_CEN;
